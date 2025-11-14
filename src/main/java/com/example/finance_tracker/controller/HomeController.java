5b731ea7d83b44bc66f343d5e5b0835ad83a4901
Felix <felix.prima@binus.ac.id>
2025-11-14 09:02:10 +0700

    initial setup

---
+++ src/main/java/com/example/finance_tracker/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.finance_tracker.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @GetMapping({"/", "/index"})
+    public String home (Model model) {
+
+        return "index";
+    }
+
+}
